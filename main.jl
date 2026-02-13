4d7c0cbedd736e66ae8d304127f013cb20f6de72
Camila Flores <camfloresfc@gmail.com>
2026-02-13 01:43:41 +0000

    Funcion saludar agregada por Estudiante B

--- main.jl
+++ main.jl
@@ -1,1 +1,10 @@
-println("Hola mundo")
+# 1. Hola Mundo
+println("Hola, Mundo desde Julia!")
+
+# 2. Definición de una función simple
+function saludar(nombre)
+    return "Hola, $nombre, bienvenido a Ciencia de Datos."
+end
+
+# 3. Llamar a la función
+println(saludar("Estudiante B"))
