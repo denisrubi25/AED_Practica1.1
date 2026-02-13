4d3537ae8153380cf1bbbc64dd99faa74ee5b13b
denisrubi25 <cb003.denisrubi.muro@gmail.com>
2026-02-12 20:52:06 -0600

    Update main.jl

--- main.jl
+++ main.jl
@@ -1,4 +1,12 @@
-println("Hola mundo")
+# 1. Hola Mundo
+println("Hola, Mundo desde Julia!")
+
+# 2. Función suma
 function suma(a, b)
     return a + b
+end
+
+# 3. Función saludar
+function saludar(nombre)
+    return "Hola, $nombre, bienvenido a Ciencia de Datos."
 end
