2a1c1b84386ac908c7025d46f94996ba1d0fd1e6
denisrubi25 <cb003.denisrubi.muro@gmail.com>
2026-02-13 03:31:31 +0000

    Agregando archivo main.jl con Hola Mundo

--- main.jl
+++ main.jl
@@ -1,8 +1,1 @@
-# Version modificada
-println("Hola, Mundo desde Julia B")
-
-function saludar(nombre)
-    return "Hola, $nombre, bienvenida a Ciencia de Datos."
-end
-
-println(saludar("Estudiante B"))
+println("Hola mundo")
