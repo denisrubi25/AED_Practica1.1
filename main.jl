4b6e0ef4d0787b9b7ec20c7e218c952c0ae9d1e6
denisrubi25 <cb003.denisrubi.muro@gmail.com>
2026-02-13 02:30:08 +0000

    Agregando archivo main.jl con Hola Mundo

--- main.jl
+++ main.jl
@@ -1,10 +1,1 @@
-# 1. Hola Mundo
-println("Hola, Mundo desde Julia!")
-
-# 2. Definición de una función simple
-function saludar(nombre)
-    return "Hola, $nombre, bienvenido a Ciencia de Datos."
-end
-
-# 3. Llamar a la función
-println(saludar("Estudiante B"))
+println("Hola mundo")
